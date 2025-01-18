bad6cfd8a368c79e0e401d0b59843b55589b6f05
dand90 <bleach90@gmail.com>
2025-01-17 17:17:58 -0700

    B: Created Java packages, layouts folder and images

--- ats/src/main/java/com/gcu/ats/AtsApplication.java
+++ ats/src/main/java/com/gcu/ats/AtsApplication.java
@@ -1,8 +1,10 @@
 package com.gcu.ats;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.gcu"})
 @SpringBootApplication
 public class AtsApplication {
 
