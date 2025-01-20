02b788730eaaca2084501cb9f6729b1d0c057b41
dand90 <bleach90@gmail.com>
2025-01-19 22:09:24 -0700

    G: Added register and relogin controllers, and UserModel (WhimsicalGander)

--- ats/src/main/java/com/gcu/controller/LoginController.java
+++ ats/src/main/java/com/gcu/controller/LoginController.java
@@ -8,13 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.gcu.model.LoginModel;
 import com.gcu.model.ProductModel;
+import com.gcu.model.UserModel;
 
 import javax.validation.Valid;
 
+//Master Controller 
 @Controller
 public class LoginController {
+	//Controller for the login page
 	@GetMapping("/login")
 	public String display(Model model) {
 		model.addAttribute("title", "Login Form");
@@ -22,13 +27,38 @@ public class LoginController {
 		return "login";
 		
 	}
+
+	//Controller for the page after Registration to redirect to login
+	@PostMapping("/login/reLogin")
+	public String reLogin(@Valid LoginModel loginModel, BindingResult bindingresult, Model model) {
+		//check for errors in the register form
+		if (bindingresult.hasErrors()) {
+			model.addAttribute("title", "Register");
+			model.addAttribute("loginModel", new UserModel());
+			return "register";
+		}
+		
+		model.addAttribute("title", "return");
+		return "reLogin";	
+	}
+		
+	//Controller for the register page
+	@GetMapping("/login/register")
+	public String register(Model model) {
+			
+		model.addAttribute("title", "Register");
+		model.addAttribute("loginModel", new UserModel());
+		
+		return "register";
+	}
+	//Controller for doLogin, displays contents of products (Job Postings)
 	@PostMapping("/doLogin")
 	public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model) {
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("title", "Login Form");
 			return "login";
 		}
-		
+
 		List<ProductModel> products = new ArrayList<ProductModel>();
 		products.add(new ProductModel(0L, "Head Chef", "Sushi Restaurant", 34.00f, 1));
 		products.add(new ProductModel(1L, "Cook", "Pizza Palace", 23.00f, 2));
@@ -40,6 +70,24 @@ public class LoginController {
 		model.addAttribute("products", products);
 		
 		return "products";
+
+	/* 
+	//Controller to direct to the main application
+	@PostMapping("/mainApplication")
+	public String mainApplication(@Valid LoginModel loginmodel, BindingResult bindingresult, Model model) {
+		
+		//check for errors in the login form
+		if (bindingresult.hasErrors()) {
+			model.addAttribute("title", "Login Form");
+			return "login";
+		}
+		model.addAttribute("title", "Main Application");
+		model.addAttribute("users", new LoginModel());
+		
+		
+		return "mainApplication";
+	} */
+		
 	}
 
 }
