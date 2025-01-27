5ae33575c6967472eeefd21df87189ff80efc736
WhimsicalGander <carterkemp31@gmail.com>
2025-01-26 18:37:11 -0800

    Update LoginController.java

--- ats/src/main/java/com/gcu/controller/LoginController.java
+++ ats/src/main/java/com/gcu/controller/LoginController.java
@@ -33,18 +33,20 @@ public class LoginController {
 			model.addAttribute("title", "Login Form");
 			return "login";
 		}
-
-		List<ProductModel> products = new ArrayList<ProductModel>();
-		products.add(new ProductModel(0L, "Head Chef", "Sushi Restaurant", 34.00f, 1));
-		products.add(new ProductModel(1L, "Cook", "Pizza Palace", 23.00f, 2));
-		products.add(new ProductModel(2L, "Kitchen Staff", "Sunday's", 17.00f, 3));
-		products.add(new ProductModel(3L, "Server", "Sushi Restaurant", 15.00f, 4));
-		products.add(new ProductModel(4L, "Host", "Sunday's", 15.00f, 2));
+		if(security.authenticate(loginmodel.getUsername(), loginmodel.getPassword())) {
+			List<ProductModel> products = new ArrayList<ProductModel>();
+			products.add(new ProductModel(0L, "Head Chef", "Sushi Restaurant", 34.00f, 1));
+			products.add(new ProductModel(1L, "Cook", "Pizza Palace", 23.00f, 2));
+			products.add(new ProductModel(2L, "Kitchen Staff", "Sunday's", 17.00f, 3));
+			products.add(new ProductModel(3L, "Server", "Sushi Restaurant", 15.00f, 4));
+			products.add(new ProductModel(4L, "Host", "Sunday's", 15.00f, 2));
 		
-		model.addAttribute("title", "My Job Postings");
-		model.addAttribute("products", products);
+			model.addAttribute("title", "My Job Postings");
+			model.addAttribute("products", products);
 		
-		return "products";
+			return "products";
+		}
+		else return "login";
 		
 	}
 
