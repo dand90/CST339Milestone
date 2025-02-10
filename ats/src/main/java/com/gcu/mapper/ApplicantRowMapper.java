24e634f9b4ea5ed8b6b7c3ba520dfead1c4420bd
WhimsicalGander <carterkemp31@gmail.com>
2025-02-09 21:12:14 -0800

    Update ApplicantRowMapper.java

--- ats/src/main/java/com/gcu/mapper/ApplicantRowMapper.java
+++ ats/src/main/java/com/gcu/mapper/ApplicantRowMapper.java
@@ -12,7 +12,7 @@ public class ApplicantRowMapper implements RowMapper<ApplicantsEntity> {
     //mapRow Method: maps UserEntity to MySQL columns
     @Override
     public ApplicantsEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
-       return new ApplicantsEntity(rs.getLong("id"), rs.getString("name"), rs.getString("phone"), rs.getString("history"), rs.getString("skills"), rs.getLong("jobPosting_id"));
+       return new ApplicantsEntity(rs.getLong("id"), rs.getString("name"), rs.getString("phone"), rs.getString("history"), rs.getString("skills"), rs.getLong("jobposting_id"));
     }
 
 }
