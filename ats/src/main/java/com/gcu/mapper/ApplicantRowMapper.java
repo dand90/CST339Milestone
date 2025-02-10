62b0f7c8e4c46868210530cf79185b0197d3ef1d
WhimsicalGander <carterkemp31@gmail.com>
2025-02-09 19:55:37 -0800

    Add files via upload

---
+++ ats/src/main/java/com/gcu/mapper/ApplicantRowMapper.java
@@ -0,0 +1,18 @@
+package com.gcu.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.gcu.entity.ApplicantsEntity;
+
+//UserRowMapper Class
+public class ApplicantRowMapper implements RowMapper<ApplicantsEntity> {
+    //mapRow Method: maps UserEntity to MySQL columns
+    @Override
+    public ApplicantsEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+       return new ApplicantsEntity(rs.getLong("id"), rs.getString("name"), rs.getString("phone"), rs.getString("history"), rs.getString("skills"), rs.getLong("jobPosting_id"));
+    }
+
+}
