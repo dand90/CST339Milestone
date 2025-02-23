6d23fd02a1a60d7d14dc6799d1e897b7b6975d22
WhimsicalGander <carterkemp31@gmail.com>
2025-02-23 12:55:18 -0800

    Update UserDataService.java

--- ats/src/main/java/com/gcu/data/UserDataService.java
+++ ats/src/main/java/com/gcu/data/UserDataService.java
@@ -1,75 +1,62 @@
 package com.gcu.data;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gcu.entity.UserEntity;
-import com.gcu.repository.UserRepository;
+import com.gcu.repository.UsersRepository;
 
-//UserDataService implements DataAccessInterface for CRUD Operations, mapped to UserRepository
 @Service
-public class UserDataService implements DataAccessInterface<UserEntity> {
+public class UsersDataService implements UsersDataAccessInterface <com.gcu.entity.UserEntity>, DataAccessInterface <com.gcu.entity.UserEntity>{
 
-    @Autowired
-    private UserRepository usersRepository;
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    public UserDataService(UserRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
-    
-    //findAll selects and returns all rows in table
-    @Override
-    public List<UserEntity> findAll() {
-       List<UserEntity> users = new ArrayList<>();
-       try {
-        Iterable<UserEntity> usersIterable = usersRepository.findAll();
-        users = new ArrayList<UserEntity>();
-        usersIterable.forEach(users::add);
-       }
-       catch (Exception e)
-       {
-        e.printStackTrace();
-       }
-       return users;
-    }
+	@Override
+	public UserEntity findByUsername(String username) {
+		
+		return usersRepository.findByUsername(username);
+	}
 
-    //findById (WIP)
-    @Override
-    public UserEntity findById(int id) {
-    	try {
-            // Find by ID using the repository
-            return usersRepository.findById((long) id).orElse(null);  // Convert 'id' to 'Long'
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;  // Return null if something goes wrong
-        }
-    }
+	@Override
+	public UserEntity findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    //create inserts a user and returns boolean
-    @Override
-    public boolean create(UserEntity user) {
-        try {
-            this.usersRepository.save(user);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean create(UserEntity t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    //update (WIP)
-    @Override
-    public boolean update(UserEntity t) {
-        return true;
-    }
+	@Override
+	public boolean update(UserEntity t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    //delete (WIP)
-    @Override
-    public boolean delete(UserEntity t) {
-        return true;
-    }
+	@Override
+	public boolean delete(UserEntity t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<UserEntity> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
